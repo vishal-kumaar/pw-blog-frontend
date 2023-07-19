98a4022078f73153fad0be377057790c034bcdf9
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-19 13:40:20 +0530

    Update BlogCard and Navbar

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,23 +1,31 @@
 import React from "react";
 
-export default function BlogCard({title, desc, imgUrl}) {
+export default function BlogCard({ title, desc, imgUrl, isEdit, isDelete }) {
   return (
     <div
-      className={`text-center w-[calc(33.33%-8px)] h-fit bg-[#282828] px-6 pt-4 pb-2 rounded-lg`}>
-      <img
-        src={imgUrl}
-        alt="cover"
-        className={`w-full h-52 block`}
-      />
-      <h1 className={`font-bold mt-4 text-lg overflow-y-auto h-14`}>
+      className={`text-center w-[calc(33.33%-8px)] h-fit bg-[#282828] px-6 py-8 rounded-lg`}>
+      <img src={imgUrl} alt="cover" className={`w-full h-52 block`} />
+      <h1 className={`font-bold mt-4 text-lg overflow-y-auto h-14 break-all`}>
         {title}
       </h1>
-      <p className={`font-normal text-lg overflow-y-auto h-28 my-3`}>
+      <p className={`font-normal text-lg overflow-y-auto h-28 my-3 break-all`}>
         {desc}
       </p>
       <button
         className={`bg-[#EC5131] hover:bg-[#F26D49] w-full rounded-lg py-1.5 px-2 mt-3 text-lg text-white`}>
         Read
+      </button>
+      <button
+        className={`${
+          isEdit ? "block" : "hidden"
+        } bg-[#EC5131] hover:bg-[#F26D49] w-full rounded-lg py-1.5 px-2 mt-3 text-lg text-white`}>
+        Edit
+      </button>
+      <button
+        className={`${
+          isDelete ? "block" : "hidden"
+        } bg-[#EC5131] hover:bg-[#F26D49] w-full rounded-lg py-1.5 px-2 mt-3 text-lg text-white`}>
+        Delete
       </button>
     </div>
   );
