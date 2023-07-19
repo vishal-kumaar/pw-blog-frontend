6a59cd232065b877b60d58e236fb281a4bec1874
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-19 12:59:00 +0530

    Create home page

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 export default function Routes() {
   const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
     {
       path: "/login",
       element: <Login />,
---
+++ src/pages/Home/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import BlogCard from "../../components/BlogCard";
+
+export default function Home() {
+  return (
+    <main
+      className={`text-[#B9B9B9] flex flex-wrap gap-x-3 gap-y-6 max-w-[70rem] mx-auto my-7`}>
+      <BlogCard
+        title="Learn web development in 10 days on PW Skills"
+        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
+          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dignissimos, officiis."
+        imgUrl="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200214165928/Web-Development-Course-Thumbnail.jpg"
+      />
+      <BlogCard
+        title="Learn web development in 10 days on PW Skills"
+        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
+          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dignissimos, officiis."
+        imgUrl="https://elements-cover-images-0.imgix.net/9a8a9942-4c73-45c7-bf65-47056f3e0e40?auto=compress%2Cformat&fit=max&w=900&s=fdfa7dd544dd208c6ab481ca35abe3ff"
+      />
+      <BlogCard
+        title="Learn web development in 10 days on PW Skills"
+        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
+          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dignissimos, officiis."
+        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
+      />
+      <BlogCard
+        title="Learn web development in 10 days on PW Skills"
+        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
+          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dignissimos, officiis."
+        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
+      />
+      <BlogCard
+        title="Learn web development in 10 days on PW Skills"
+        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
+          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dignissimos, officiis."
+        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
+      />
+    </main>
+  );
+}
