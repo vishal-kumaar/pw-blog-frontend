b2843ada7ca179798dfdab6e72bedce4fd1b0331
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 11:37:11 +0530

    Connect login page to api

---
+++ src/apis/loginUser.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const loginUser = async (data) => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/api/user/login`,
+      data
+    );
+
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export default loginUser;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import loginUser from "../../apis/loginUser";
+import { toast } from "react-toastify";
+import TokenContext from "../../contexts/token/TokenContext";
 
 export default function Login() {
+  const { setUserToken } = useContext(TokenContext);
+  const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeUserData = (fieldName, value) => {
+    setUserData({
+      ...userData,
+      [fieldName]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await loginUser(userData);
+    if (res.success) {
+      setUserToken(res.token);
+      toast(res.message, {
+        position: "top-right",
+        type: "success",
+        theme: "dark",
+        autoClose: 3000,
+      });
+      navigate("/", { replace: true });
+    } else {
+      toast(res.message, {
+        position: "top-right",
+        type: "error",
+        theme: "dark",
+        autoClose: 3000,
+      });
+    }
+
+    setUserData({
+      password: "",
+      email: "",
+    });
+  };
+
   return (
     <main
       className={`w-[90%] sm:w-96 mt-10 mx-auto bg-[#393939] px-5 py-8 rounded-xl`}>
       <h1 className="text-white text-center font-bold text-3xl sm:text-4xl mb-10 mt-4">
         Login
       </h1>
-      <form className={`flex flex-col gap-3`}>
+      <form className={`flex flex-col gap-3`} onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Email"
           className={`text-base border-none outline-[#EC4F2F] rounded-xl py-2.5 px-3 w-full text-[#AA8F7F] placeholder:text-[#AA8F7F]`}
+          value={userData.email}
+          onChange={(event) => changeUserData("email", event.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className={`text-base border-none outline-[#EC4F2F] rounded-xl py-2.5 px-3 w-full text-[#AA8F7F] placeholder:text-[#AA8F7F]`}
+          value={userData.password}
+          onChange={(event) => changeUserData("password", event.target.value)}
         />
         <button
           type="submit"
