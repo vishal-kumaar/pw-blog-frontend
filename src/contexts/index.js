de36751cfa46a1306968debd1bd9360919c22daf
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 10:59:42 +0530

    Create token context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import TopbarProvider from "./topbar/TopbarProvider";
-import EditBlogProvider from "./editBlog/EditBlogProvider.js"
+import EditBlogProvider from "./editBlog/EditBlogProvider.js";
+import TokenProvider from "./token/TokenProvider";
 
 export default function Context({ children }) {
   return (
-    <TopbarProvider>
-      <EditBlogProvider>{children}</EditBlogProvider>
-    </TopbarProvider>
+    <TokenProvider>
+      <TopbarProvider>
+        <EditBlogProvider>{children}</EditBlogProvider>
+      </TopbarProvider>
+    </TokenProvider>
   );
 }
