804d9fb88bd31f281f3b1381c4ee96ca09bbc4dc
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 15:29:49 +0530

    Add delete blog api

---
+++ src/apis/deleteBlog.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const deleteblog = async (blogId, token) => {
+  try {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_BASE_URL}/api/blog/${blogId}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export default deleteblog;
