ec64499cd1a5243df2a48dc3ba02008400b427a2
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 12:28:14 +0530

    Connect add home page to api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BlogCard from "../../components/BlogCard";
+import getAllBlogs from "../../apis/getAllBlogs";
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  const handleBlog = async () => {
+    const res = await getAllBlogs();
+
+    if (res.success) {
+      setBlogs(res.blogs);
+    }
+  };
+
+  useEffect(() => {
+    handleBlog();
+  }, []);
+
   return (
     <main
       className={`text-[#B9B9B9] flex flex-wrap gap-x-3 gap-y-6 w-[90%] sm:w-[95%] xl:w-[70rem] mx-auto my-7`}>
-      <BlogCard
-        title="Learn web development in 10 days on PW Skills"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
-          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dignissimos, officiis."
-        imgUrl="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200214165928/Web-Development-Course-Thumbnail.jpg"
-      />
-      <BlogCard
-        title="Learn web development in 10 days on PW Skills"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
-          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dignissimos, officiis."
-        imgUrl="https://elements-cover-images-0.imgix.net/9a8a9942-4c73-45c7-bf65-47056f3e0e40?auto=compress%2Cformat&fit=max&w=900&s=fdfa7dd544dd208c6ab481ca35abe3ff"
-      />
-      <BlogCard
-        title="Learn web development in 10 days on PW Skills"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
-          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dignissimos, officiis."
-        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
-      />
-      <BlogCard
-        title="Learn web development in 10 days on PW Skills"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
-          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dignissimos, officiis."
-        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
-      />
-      <BlogCard
-        title="Learn web development in 10 days on PW Skills"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officiis
-          ducimus tenetur veritatis numquam nemo. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dignissimos, officiis."
-        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hZEF-P1-tGcAaFwMmzK4kaRFl3lzvy0YwIn3xQffOAIl9mAcFtws7IY59q-AArDD0MA&usqp=CAU"
-      />
+      {blogs.map((blog) => (
+        <BlogCard key={blog._id} blog={blog} isEdit={false} isDelete={false} />
+      ))}
     </main>
   );
 }
