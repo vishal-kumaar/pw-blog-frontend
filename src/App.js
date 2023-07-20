22e7cace106b2851047378b265ff84e07dc23ecc
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 11:26:15 +0530

    Add react-toastify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Context from "./contexts";
 import Navbar from "./components/Navbar";
 import Topbar from "./components/Topbar";
 import EditBlog from "./components/EditBlog";
-import Context from "./contexts";
+import Routes from "./Routes";
 
 function App() {
   document.body.classList.add("bg-black");
   return (
     <BrowserRouter>
       <Context>
+        <ToastContainer />
         <Navbar />
         <Topbar />
         <EditBlog />
