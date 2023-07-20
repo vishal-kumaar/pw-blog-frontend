05d34314f82116418aa02ad8ecedde5d2ceb44e8
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 11:57:33 +0530

    Add addBlog api

---
+++ src/apis/addBlog.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const addBlog = async (data, token) => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/api/blog/add`,
+      data,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export default addBlog;
