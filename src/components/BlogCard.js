dcad5a6ffca2b5685e270aa8861e0eb9e0be0a94
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 12:59:22 +0530

    Fix Blog cards component

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,32 +1,32 @@
 import React, { useContext } from "react";
 import EditBlogContext from "../contexts/editBlog/EditBlogContext";
+import imagePlaceholder from "../assets/icons/image_placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
-export default function BlogCard({
-  id,
-  title,
-  desc,
-  imgUrl,
-  isEdit,
-  isDelete,
-}) {
+export default function BlogCard({ blog, isEdit, isDelete }) {
   const { edit } = useContext(EditBlogContext);
+  const navigate = useNavigate();
 
   return (
     <div
       className={`text-center w-full sm:w-[calc(50%-8px)] md:w-[calc(33.33%-8px)] h-fit bg-[#282828] px-6 py-8 rounded-lg`}>
       <img
-        src={imgUrl}
+        src={blog.imgUrl}
         alt="cover"
+        onError={(event) => {
+          event.target.src = imagePlaceholder;
+        }}
         className={`w-full sm:h-36 lg:h-52 block`}
       />
       <h1 className={`font-bold mt-4 text-lg overflow-y-auto h-14 break-word`}>
-        {title}
+        {blog.title}
       </h1>
       <p
         className={`font-normal text-lg overflow-y-auto h-28 my-3 break-words`}>
-        {desc}
+        {blog.desc}
       </p>
       <button
+        onClick={() => navigate(`/blog/${blog._id}`)}
         className={`bg-[#EC5131] hover:bg-[#F26D49] w-full rounded-lg py-1.5 px-2 mt-3 text-lg text-white`}>
         Read
       </button>
@@ -34,7 +34,7 @@ export default function BlogCard({
         className={`${
           isEdit ? "block" : "hidden"
         } bg-[#EC5131] hover:bg-[#F26D49] w-full rounded-lg py-1.5 px-2 mt-3 text-lg text-white`}
-        onClick={() => edit(id)}>
+        onClick={() => edit(blog._id)}>
         Edit
       </button>
       <button
